1bdfdf6af16697c51194457d829c4d3545b18412
tk_user <tk_user@Mac-mini-tk-admin.local>
2019-09-17 18:59:51 +0300

    update

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -43,32 +43,37 @@ const App = ({ list, completedCount, fullListlength, dispatch }: Props) => {
 
   return (
     <div className="container">
-      <AddForm />
-      {list.length ? (
-        <>
-          <List list={list} deleteItem={deleteItem} changeItem={toggleItem} editItem={editItem} />
-          <div className="info-wrapper">
-            <div className="toggle-all">
-              <label className="label" htmlFor="toggler">
-                {completedCount === list.length ? 'Uncomplete' : 'Complete'} all
-              </label>
-              <input
-                id="toggler"
-                className="checkbox"
-                type="checkbox"
-                checked={completedCount === list.length ? true : false}
-                onChange={toggleAll}
-              />
+      <div className="wrapper">
+        <AddForm />
+        {list.length ? (
+          <>
+            <List list={list} deleteItem={deleteItem} changeItem={toggleItem} editItem={editItem} />
+            <div className="info-wrapper">
+              <div className="toggle-all">
+                <label className="label" htmlFor="toggler">
+                  {completedCount === list.length ? 'Uncomplete' : 'Complete'} all
+                </label>
+                <input
+                  id="toggler"
+                  className="checkbox"
+                  type="checkbox"
+                  checked={completedCount === list.length ? true : false}
+                  onChange={toggleAll}
+                />
+              </div>
+              <div className="left-count">
+                <span className={fullListlength - completedCount === 0 ? 'count red' : 'count'}>
+                  {fullListlength - completedCount}
+                </span>{' '}
+                {fullListlength - completedCount === 1 ? 'item' : 'items'} left
+              </div>
             </div>
-            <div className="left-count">
-              {fullListlength - completedCount} {fullListlength - completedCount === 1 ? 'item' : 'items'} left
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="empty-list">List is empty</div>
-      )}
-      <Filters />
+          </>
+        ) : (
+          <div className="empty-list">List is empty</div>
+        )}
+        <Filters />
+      </div>
     </div>
   )
 }
